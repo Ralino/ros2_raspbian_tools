1f604ec4c5efa371842446fae54556d01616a7ce
Thomas Friedel <ralino96@googlemail.com>
2018-05-23 17:51:14 +0200

    Use ros2's libPoco instead of both system and ros2

--- build_ros2.bash
+++ build_ros2.bash
@@ -5,7 +5,7 @@ set -euf -o pipefail
 PYTHON_MAJOR=3
 PYTHON_MINOR=5
 
-export POCO_ROOT="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib"
+#export POCO_ROOT="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib"
 
 src/ament/ament_tools/scripts/ament.py build \
     --cmake-args \
@@ -16,9 +16,6 @@ src/ament/ament_tools/scripts/ament.py build \
     -DEIGEN3_INCLUDE_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/include/eigen3" \
     -DOpenCV_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/share/OpenCV" \
     -DEigen3_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib/cmake/eigen3" \
-    -DPoco_INCLUDE_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/include" \
-    -DPoco_LIBRARY_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib" \
-    -DPoco_LIBRARIES="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib/libPocoFoundation.so" \
     -DASIO_INCLUDE_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/include" \
     -DTINYXML2_INCLUDE_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/include" \
     -DTINYXML2_LIBRARY="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib/${RASPBERRYPI_CROSS_COMPILE_TOOLCHAIN_PREFIX}/libtinyxml2.so" \
@@ -26,3 +23,6 @@ src/ament/ament_tools/scripts/ament.py build \
     -DTinyXML_LIBRARY="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib/${RASPBERRYPI_CROSS_COMPILE_TOOLCHAIN_PREFIX}/libtinyxml.so" \
     -- \
     $*
+#    -DPoco_INCLUDE_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/include" \
+#    -DPoco_LIBRARY_DIR="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib" \
+#    -DPoco_LIBRARIES="${RASPBERRYPI_CROSS_COMPILE_SYSROOT}/usr/lib/libPocoFoundation.so" \
