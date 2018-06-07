ced78aaa7d504e588dc8e59464662e81efdb7047
Thomas Friedel <ralino96@googlemail.com>
2018-06-07 16:21:23 +0200

    Update ros2 dependencies

--- ros2_dependencies.bash
+++ ros2_dependencies.bash
@@ -11,7 +11,7 @@ apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 421C365BD9FF1F717
 
 apt update
 apt -y install git wget
-apt -y install build-essential cppcheck cmake libopencv-dev libpoco-dev python-empy python3-dev python3-empy python3-nose python3-pip python3-setuptools python3-vcstool python3-yaml libtinyxml-dev libeigen3-dev
+apt -y install build-essential cppcheck cmake libopencv-dev libpoco-dev python-empy python3-dev python3-catkin-pkg-modules python3-empy python3-nose python3-pip python3-setuptools python3-vcstool python3-yaml libtinyxml-dev libeigen3-dev
 pip3 install argcomplete
 pip3 install flake8 flake8-blind-except flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes
 apt -y install libasio-dev libtinyxml2-dev python3-yaml
